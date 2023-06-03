9cdd71af4b5af69e5b42e0b9b0c8ce95b17cd841
jonathan.G <jonathan.gouveia@protonmail.com>
2023-06-03 10:50:30 +0200

    first_commit

---
+++ src/main/java/com/openclassroomsProject/Mediscreenreport/MediscreenReportApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroomsProject.Mediscreenreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassroomsProject/Mediscreenreport/MediscreenReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroomsProject.Mediscreenreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
